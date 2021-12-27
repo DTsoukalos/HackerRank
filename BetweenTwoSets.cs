7a26a210e249006eb4331e17c74cf57635f3b99d
DTsoukalos <51972234+DTsoukalos@users.noreply.github.com>
2021-12-27 22:42:53 +0200

    Create BetweenTwoSets.cs
    Easy

---
+++ BetweenTwoSets.cs
@@ -0,0 +1,79 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        int finall=0;
+        int maxNumber = a.Max();
+        int minNumber = b.Min();
+        for(int i=1;i*maxNumber <= minNumber ;i++ ){
+            bool tempbool=true;
+            foreach(int c in a){
+                if((i*maxNumber) % c !=0){
+                    tempbool=false;
+                    break;
+                }                
+            }  
+            foreach(int c in b){
+                    if( c % (i*maxNumber)!=0){
+                    tempbool=false;
+                    break;
+                }
+            }          
+            if(tempbool){
+                finall++;
+            }
+        }
+        
+        return finall;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        textWriter.WriteLine(total);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
